948a04d4673901351582e65c33a0d29ec7f5c474
MaxO2431 <marina-osipova-82@bk.ru>
2023-06-09 12:23:28 +0300

    Create n.py

---
+++ n.py
@@ -0,0 +1,19 @@
+class Car:
+    def __init__(self, make, model, year):
+        if not isinstance(make, str) or not isinstance(model, str) or not isinstance(year, int):
+            raise TypeError("Марка и модель должны быть строками, год должен быть целым числом")
+        self.make = make
+        self.model = model
+        self.year = year
+
+    def start(self):
+        if self.year < 2010:
+            raise ValueError("Эта машина слишком старая, чтобы надежно заводиться")
+        print(f"The {self.year} {self.make} {self.model} starts")
+
+    def drive(self, distance):
+        if not isinstance(distance, (int, float)) or distance <= 0:
+            raise ValueError("Расстояние должно быть положительным числом")
+        print(f" {self.year} {self.make} {self.model} приводы {distance} km")
+
+
